de62e4752ffa3c9c2c55f20ba2523ed85c9797bc
leyenda10 <anthonyleandroid@gmail.com>
2016-04-14 15:15:26 -0500

    cree un cubo  modiique su posicion

---
+++ TRABAJO EN GRUPO/Assets/cubo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	// aqui va la info para inicar el juego
+		gameObject.transform.position = new Vector3 (1, 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		}
+}
